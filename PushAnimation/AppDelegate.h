17994381f9ea8dce9d4e6c26318926ec4132912a
lwb <lwb@lwbdemacbook-pro.local>
2016-03-24 10:14:11 +0800

    Initial Commit

---
+++ PushAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushAnimation
+//
+//  Created by lwb on 16/3/24.
+//  Copyright © 2016年 angBiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushAnimation
+//
+//  Created by lwb on 16/3/24.
+//  Copyright © 2016年 angBiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
